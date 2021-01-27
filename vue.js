0fef1b12e54362f855882a41d33187c2ae5627df
Megumin2k17 <megumin2k17@gmail.com>
2021-01-27 19:48:35 +1100

    my commit number 2

--- vue.js
+++ vue.js
@@ -12,28 +12,81 @@ const App = {
         {title: 'Роутер', text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.'},
         {title: 'Vuex', text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.'},
         {title: 'Composition', text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.'},
-      ]
+      ],
+      nextBtnStatement: ['Вперед', 'Закончить'],
+      btnsSwapped: false
     }
   },
   methods: {
+    next() {
+
+      if(this.activeIndex < this.steps.length-1) {
+       
+        this.activeIndex++
+      } else {
+        this.btnsSwapped = true
+      }
+         
+    },
     prev() {
+
+      if(this.activeIndex > 0) {
+
+        this.activeIndex--
+      }      
+      
       // когда нажимаем кнопку назад
     },
     reset() {
+
+       this.activeIndex = 0
+       this.btnsSwapped = false
+
       // начать заново
     },
     nextOfFinish() {
+      
+      if(this.isLastStep) {
+        this.$refs.nextBtn.innerHTML = this.nextBtnStatement[1]
+        
+      } else if(!this.isLastStep && !(this.$refs.nextBtn.innerHTML === this.nextBtnStatement[0])) {
+        this.$refs.nextBtn.innerHTML = this.nextBtnStatement[0]
+      }   
       // кнопка вперед или закончить
     },
     setActive(idx) {
+
+      if(!this.isBtnsSwapped) {
+        
+        this.activeIndex = idx
+      }
+      
       // когда нажимаем на определенный шаг
     }
   },
   computed: {
+    currentStep() {
+      return this.activeIndex
+    },
+    isBackBtnDisabled() {
+      return this.activeIndex === 0
+    },
+    isLastStep() {
+      return this.activeIndex === this.steps.length-1
+    },
+    isBtnsSwapped() {
+      return this.btnsSwapped
+    }
+
     // тут стоит определить несколько свойств:
     // 1. текущий выбранный шаг
     // 2. выключена ли кнопка назад
     // 3. находимся ли мы на последнем шаге
+  },
+  beforeUpdate() {
+    if(this.$refs.nextBtn) {
+      this.nextOfFinish()
+    }    
   }
 }
 
